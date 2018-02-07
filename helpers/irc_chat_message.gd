f0f5c2ef9de5140f6b050d7434f79c2e4775fd8b
drtwisted <kirill.omelchenko@gmail.com>
2018-02-07 14:22:49 +0200

    Remove old file structure

--- helpers/irc_chat_message.gd
+++
@@ -1,6 +0,0 @@
-var name
-var text
-
-func _init(name, text):
-	self.name = name
-	self.text = text
--- helpers/message_wrapper.gd
+++
@@ -1,8 +0,0 @@
-static func wrap(server_irc_message):
-	var res = preload('irc_chat_message.gd').new('', '')
-
-	res.name = server_irc_message.prefix.split('!')[0]
-	res.text = server_irc_message.params[1]
-	res.text = res.text.substr(1, res.text.length() - 1)
-
-	return res

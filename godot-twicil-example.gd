52bc30b8cc47f9af9f262d8c920371163561dc85
drtwisted <kirill.omelchenko@gmail.com>
2018-04-03 12:46:27 +0300

    Update example

--- godot-twicil-example.gd
+++ godot-twicil-example.gd
@@ -3,39 +3,14 @@ extends Node2D
 export(bool) var animate = true
 export(float) var animations_time = 1.0
 
-class FuncRefEx extends FuncRef:
-	func _init(instance, method):
-		.set_instance(instance)
-		.set_function(method)
-
-class InteractiveCommand:
-	var func_ref
-	var params_count
-
-	func _init(func_ref, params_count):
-		self.func_ref = func_ref
-		self.params_count = params_count
-
-	func call_command(params):
-		func_ref.call_func(params)
-
 onready var twicil = get_node("TwiCIL")
 onready var sprite = get_node("Sprite")
 onready var tween = get_node("Tween")
 
 const NICK = "YOUR_NICK"
 const CLIENT_ID = "YOUR_CLIENT_ID"
-const CHANNEL = "your_channel"	# Your channel name IN LOWER CASE
-const OAUTH = "oauth:YOUR_OAUTH"
-
-var interactive_commands = {
-	'move': InteractiveCommand.new(
-		FuncRefEx.new(self, '_command_move_to'), 2),
-	'rotate': InteractiveCommand.new(
-		FuncRefEx.new(self, '_command_rotate'), 1),
-	'scale': InteractiveCommand.new(
-		FuncRefEx.new(self, '_command_scale'), 2)
-}
+const CHANNEL = "YOUR_CHANNEL"	# Your channel name IN LOWER CASE
+const OAUTH = "YOUR_OAUTH"
 
 # Private methods
 func __interpolate_method(obj, method, start_value, end_value, time):
@@ -46,10 +21,9 @@ func __interpolate_method(obj, method, start_value, end_value, time):
 		Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
 	tween.start()
 
-
 func _command_move_to(params):
-	var x = float(params[0])
-	var y = float(params[1])
+	var x = float(params[1])
+	var y = float(params[2])
 
 	if not animate:
 		sprite.set_global_pos(Vector2(x, y))
@@ -59,21 +33,19 @@ func _command_move_to(params):
 		sprite, 'set_global_pos',
 		sprite.get_global_pos(), Vector2(x, y), animations_time)
 
-
 func _command_rotate(degrees):
 
 	if not animate:
-		sprite.set_rotd(float(degrees[0]))
+		sprite.set_rotd(float(degrees[1]))
 		return
 
 	__interpolate_method(
 		sprite, 'set_rotd',
-		sprite.get_rotd(), float(degrees[0]), animations_time)
-
+		sprite.get_rotd(), float(degrees[1]), animations_time)
 
 func _command_scale(params):
-	var scale_x = float(params[0])
-	var scale_y = float(params[1])
+	var scale_x = float(params[1]) if params.size() > 1 else 1
+	var scale_y = float(params[2]) if params.size() > 2 else 1
 
 	if not animate:
 		sprite.set_scale(Vector2(scale_x, scale_y))
@@ -89,25 +61,21 @@ func connect():
 	twicil.connect_to_twitch_chat()
 	twicil.connect_to_channel(CHANNEL, CLIENT_ID, OAUTH, NICK)
 
+func send_greating_help():
+	twicil.send_message(
+		"Hi, Chat! You can use the following commands now: " +
+		"move [x] [y] - to move; rotate [degrees] - to rotate; " +
+		"scale [no_params] or [x] [y] - to scale (to 1 1 without params)")
+
+func init_interactive_commands():
+	twicil.commands.add("move", self, "_command_move_to", 2)
+	twicil.commands.add("rotate", self, "_command_rotate")
+	twicil.commands.add("scale", self, "_command_scale", 2, true)
+
 # Hooks
 func _ready():
-	twicil.connect("message_recieved", self, "_on_message_recieved")
+	init_interactive_commands()
 	twicil.set_logging(true)
 	connect()
+	send_greating_help()
 
-# Events
-func _on_message_recieved(sender, text):
-	var input_cmd = text.split(' ')
-
-	for cmd in interactive_commands:
-		if input_cmd[0] == cmd:
-
-			if input_cmd.size() - 1 < interactive_commands[cmd].params_count:
-				# TODO: React to invalid command params in chat
-				return
-
-			var params = []
-			for i in range(interactive_commands[cmd].params_count):
-				params.append(input_cmd[i + 1])
-
-			interactive_commands[cmd].call_command(params)

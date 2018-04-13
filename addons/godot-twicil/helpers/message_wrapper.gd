c6c5da3bb5d6131c3ea29944a3e20876fb0ac63f
drtwisted <kirill.omelchenko@gmail.com>
2018-04-13 11:04:15 +0300

    Add get_sender_name to message_wrapper

--- addons/godot-twicil/helpers/message_wrapper.gd
+++ addons/godot-twicil/helpers/message_wrapper.gd
@@ -1,8 +1,11 @@
 static func wrap(server_irc_message):
 	var res = preload('irc_chat_message.gd').new('', '')
 
-	res.name = server_irc_message.prefix.split('!')[0]
+	res.name = get_sender_name(server_irc_message)
 	res.text = server_irc_message.params[1]
 	res.text = res.text.substr(1, res.text.length() - 1)
 
 	return res
+
+static func get_sender_name(server_irc_message):
+	return server_irc_message.prefix.split('!')[0]
